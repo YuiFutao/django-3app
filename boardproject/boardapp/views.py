fbe0985c741c6f9d08c61af61569d1dd50b05085
YuiFutao <yui.futao@gmail.com>
2020-02-12 19:17:11 +0900

    signup enabled
    `56. 登録データの重複を防ぐ方法`まで実施した

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signupfunc(request):
+  if 'POST' == request.method:
+    username2 = request.POST['username'] #htmlの中で「name="username"」として定義した値を取得
+    password2 = request.POST['password']
+
+    #既に同じ名前のユーザが存在した場合
+    try:
+      User.objects.get(username=username2)
+      return render(request, 'signup.html', {'error':'このユーザは登録されています'} )
+
+    # 重複が無かった場合(=正常にユーザ登録できる)
+    except:
+      user = User.objects.create_user(username2, '', password2)
+      return render(request, 'signup.html', {'some':100})
+
+  return render(request, 'signup.html', {'some':100})
