ee630611dcee27fc45a87da70562c8207a25bd7c
YuiFutao <yui.futao@gmail.com>
2020-02-25 20:00:49 +0900

    画像のアップロードに対応させた
    ちなみに管理者アカウントはadmin

--- boardproject/boardproject/settings.py
+++ boardproject/boardproject/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# 画像のデフォルト保存先を指定
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# 画像のURLを指定
+MEDIA_URL = '/media/'
+
